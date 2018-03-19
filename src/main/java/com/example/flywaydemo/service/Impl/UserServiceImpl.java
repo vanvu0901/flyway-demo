41516893e13bb916712f7a95f1c859adf417b728
Vu Nguyen <35519964+vanvu0901@users.noreply.github.com>
2018-03-20 00:05:24 +0700

    Update UserServiceImpl.java
    implement updateUser

--- src/main/java/com/example/flywaydemo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/flywaydemo/service/Impl/UserServiceImpl.java
@@ -3,5 +3,8 @@ package com.example.flywaydemo.service.Impl;
 import com.example.flywaydemo.service.UserService;
 
 public class UserServiceImpl implements UserService {
-
+  
+  public updateUser(User user) throws Exception {
+    
+  }
 }
