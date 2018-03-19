4c57febeb07fbb208d7961bc7d61b6ab1639a958
vanvu0901 <35519964+vanvu0901@users.noreply.github.com>
2018-03-19 23:36:13 +0700

    implement userservice

--- src/main/java/com/example/flywaydemo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/flywaydemo/service/Impl/UserServiceImpl.java
@@ -1,4 +1,6 @@
 package com.example.flywaydemo.service.Impl;
 
-public class UserServiceImpl {
+import com.example.flywaydemo.service.UserService;
+
+public class UserServiceImpl implements UserService {
 }
