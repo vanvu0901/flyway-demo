e305b58776dcecdc2e685944032a978b746e13d3
vanvu0901 <35519964+vanvu0901@users.noreply.github.com>
2018-03-19 23:45:30 +0700

    createUser

--- src/main/java/com/example/flywaydemo/service/UserService.java
+++ src/main/java/com/example/flywaydemo/service/UserService.java
@@ -1,4 +1,10 @@
 package com.example.flywaydemo.service;
 
+import com.example.flywaydemo.domain.User;
+
 public class UserService {
+
+    public createUser(User user) throw Exception {
+
+    }
 }
