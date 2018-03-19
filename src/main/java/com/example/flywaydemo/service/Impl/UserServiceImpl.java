2936a83fac368b5171db9b03e593e3ecbeaf12b3
vanvu0901 <35519964+vanvu0901@users.noreply.github.com>
2018-03-20 00:08:15 +0700

    @RestController

--- src/main/java/com/example/flywaydemo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/flywaydemo/service/Impl/UserServiceImpl.java
@@ -1,10 +1,16 @@
 package com.example.flywaydemo.service.Impl;
 
 import com.example.flywaydemo.service.UserService;
+import com.example.flywaydemo.domain.User;
 
 public class UserServiceImpl implements UserService {
-  
+
+    @Override
+    public User createUser(User user) throws Exception {
+
+    }
+
   public updateUser(User user) throws Exception {
-    
+
   }
 }
